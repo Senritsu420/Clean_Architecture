1b47b9a90866bc444e06d5c3d10b37a1877c5d51
Senritsu420 <kubo3900@gmail.com>
2023-12-10 14:09:37 +0900

    UserのSignUp、LogIn、LogOut追加

--- backend/app/db/db.go
+++ backend/app/db/db.go
@@ -4,11 +4,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+var db *gorm.DB
+
+// dbコンテナが立ち上がるまで接続を行う
+func RetryConnectDB(dialector gorm.Dialector, opt gorm.Option, count uint) error {
+	var err error
+	for count > 1 {
+		if db, err = gorm.Open(dialector, opt); err != nil {
+			time.Sleep(time.Second * 2)
+			count--
+			fmt.Printf("retry... coutn:%v\n", count)
+			continue
+		}
+		break
+	}
+	return err
+}
 
 func NewDB() *gorm.DB {
 	// dev環境の場合envファイル読み込み、エラー時強制終了
@@ -20,10 +38,10 @@ func NewDB() *gorm.DB {
 	}
 	// MySQLに接続
 	dsn := fmt.Sprintf(`%s:%s@tcp(db:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local`, os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_DATABASE"))
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
+	if err := RetryConnectDB(mysql.Open(dsn), &gorm.Config{}, 100); err != nil {
 		log.Fatalln(err)
 	}
+
 	fmt.Println("Connected")
 	return db
 }
--- backend/app/main.go
+++ backend/app/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"app/controller"
+	"app/db"
+	"app/repository"
+	"app/router"
+	"app/usecase"
+)
 
 func main() {
-	fmt.Println("Hello golang from docker!")
+	db := db.NewDB()
+	userRepository := repository.NewUserRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userController := controller.NewUserController(userUsecase)
+	e := router.NewRouter(userController)
+	e.Logger.Fatal(e.Start(":8080"))
 }
