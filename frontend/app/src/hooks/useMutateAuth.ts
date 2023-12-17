150ed8f110301406359c9e19498b05cb93301b07
Senritsu420 <kubo3900@gmail.com>
2023-12-17 11:47:27 +0900

    ログイン関連をMutationで管理

---
+++ frontend/app/src/hooks/useMutateAuth.ts
@@ -0,0 +1,58 @@
+import { useNavigate } from "react-router-dom"
+import useStore from "../store"
+import { useError } from "./useError"
+import { useMutation } from "@tanstack/react-query"
+import axios from "axios"
+
+export  const useMutateAuth = () => {
+  const navigate = useNavigate()
+  const resetEditedTask = useStore((state)=> state.resetEditedTask)
+  const { switchErrorHandling } = useError()
+  const loginMutation = useMutation(
+    async (user: Credential) => await axios.post(`${process.env.REACT_APP_API_URL}/login`,user), {
+      onSuccess: () => {
+        navigate('/todo')
+      },
+      onError: (err: any) => {
+        // csrf middleware関連はエラーメッセージの階層が異なる
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+
+  const registerMutation = useMutation(
+    async (user: Credential) => await axios.post(`${process.env.REACT_APP_API_URL}/signup`, user), {
+      onError: (err: any) => {
+        // csrf middleware関連はエラーメッセージの階層が異なる
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+
+  const logoutMutation = useMutation(
+    async (user: Credential) => await axios.post(`${process.env.REACT_APP_API_URL}/logout`,user), {
+      onSuccess: () => {
+        resetEditedTask()
+        navigate('')
+      },
+      onError: (err: any) => {
+        // csrf middleware関連はエラーメッセージの階層が異なる
+        if (err.response.data.message) {
+          switchErrorHandling(err.response.data.message)
+        } else {
+          switchErrorHandling(err.response.data)
+        }
+      },
+    }
+  )
+
+  return { loginMutation, registerMutation, logoutMutation }
+}
